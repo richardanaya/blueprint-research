a1ba830aa64343a4e70ad39b98ee292c14db9fb1
Richard Anaya <richard.anaya@gmail.com>
2026-02-01 12:10:12 -0800

    fix

--- blueprint-ingredients/next.config.ts
+++ blueprint-ingredients/next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'dist',
-  assetPrefix: '.',
+  basePath: '/blueprint-research/blueprint-ingredients/dist',
+  assetPrefix: '/blueprint-research/blueprint-ingredients/dist',
   images: {
     unoptimized: true,
   },
